125c90efc10c217b9777ae49ee32e96e594b4aff
LPCBCN044 <alexmrtc@gmail.com>
2024-04-16 10:44:28 +0200

    Complete all tests

--- src/Viking.ts
+++ src/Viking.ts
@@ -1,1 +1,24 @@
-export class Viking{}
+import { Soldier } from "./Soldier";
+
+export class Viking extends Soldier{
+    name: string;
+
+    constructor(name:string, health:number, strength:number){
+        super(health, strength);
+
+        this.name = name;
+    }
+
+    receiveDamage(damage: number) {
+        this.health -= damage;
+
+        if(this.health <= 0){
+            return `${this.name} has died in act of combat`
+        }
+        return `${this.name} has received ${damage} points of damage`
+    }
+
+    battleCry () {
+        return "Odin Owns You All!"
+    }
+}
--- src/War.ts
+++ src/War.ts
@@ -1,1 +1,40 @@
-export class War {}
+import { Saxon } from "./Saxon";
+import { Viking } from "./Viking";
+
+export class War {
+    vikingArmy:Viking[] = [];
+    saxonArmy:Saxon[] = [];
+
+    addViking(viking: Viking) {
+        this.vikingArmy.push(viking);
+    }
+
+    addSaxon(saxon:Saxon){
+        this.saxonArmy.push(saxon);
+    }
+
+    vikingAttack(){
+        this.saxonArmy[0].receiveDamage(this.vikingArmy[0].strength);
+
+        if(this.saxonArmy[0].health <= 0){
+            this.saxonArmy.pop()
+            return "A Saxon has died in combat"
+        }
+    }
+
+    saxonAttack() {
+        this.vikingArmy[0].receiveDamage(this.saxonArmy[0].strength);
+
+        if(this.vikingArmy[0].health <= 0){
+            this.vikingArmy.pop();
+        } else {
+            return `${this.vikingArmy[0].name} has received ${this.saxonArmy[0].strength} points of damage`;
+        }
+    }
+
+    showStatus () {
+        if(this.saxonArmy.length <= 0){
+            return "Vikings have won the war of the century!"
+        }
+    }
+}
